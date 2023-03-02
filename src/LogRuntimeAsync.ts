93e02cf3a830fa2a4edb47ba494fed2f42dc6345
Shachar Har-Shuv <shachar.harshuv@gmail.com>
2023-03-02 16:36:26 -0500

    Fix incorrect this reference issue

--- src/LogRuntimeAsync.ts
+++ src/LogRuntimeAsync.ts
@@ -3,9 +3,9 @@ import { createMethodDecoratorFromHighOrderFn } from "./createMethodDecoratorFro
 export const LogRuntimeAsync = (label?: string) =>
   createMethodDecoratorFromHighOrderFn(
     (fn: (...args: any[]) => Promise<any>) => {
-      return async (...args: any[]) => {
+      return async function (this: any, ...args: any[]) {
         const start = performance.now();
-        const result = await fn(...args);
+        const result = await fn.call(this, ...args);
         const end = performance.now();
         console.log(`${label || "Async method"} took ${end - start} ms`);
         return result;
